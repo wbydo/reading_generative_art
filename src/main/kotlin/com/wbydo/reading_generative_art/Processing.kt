49456232063b6b11c8d85e21a97e818410d50a95
wbydo <wbydo@users.noreply.github.com>
2019-07-16 23:50:59 +0900

    curveVertexを使って書き直し

--- src/main/kotlin/com/wbydo/reading_generative_art/Processing.kt
+++ src/main/kotlin/com/wbydo/reading_generative_art/Processing.kt
@@ -29,21 +29,20 @@ class Processing : PApplet () {
         stroke(20f, 50f, 70f)
         noFill()
 
-        var prevX = x0
-        var prevY = y0
         var prevR = 0f
 
         val amp = random(100f)
+
+        beginShape()
         for (theta in 0..(360 * maxRound) step dTheta) {
             val rad = radians(theta.toFloat())
             val x = x0 + prevR * cos(rad)
             val y = y0 + prevR * sin(rad)
-            line(prevX, prevY, x, y)
+            curveVertex(x, y)
 
             prevR = prevR + dr + (random(1f) - 0.5f) * amp
-            prevX = x
-            prevY = y
         }
+        endShape()
     }
 
     fun run(args: Array<String>) : Unit
